f3fa385ccb921249fd8db1bf10330c8f2b5b839c
Paul Smith <prsmith@ranken.edu>
2020-12-30 17:04:38 -0600

    Creating project from basic activity template and fixing a few things.

---
+++ 01.4/BasicActivity/app/src/main/java/edu/ranken/prsmith/basicactivity/MainActivity.java
@@ -0,0 +1,56 @@
+package edu.ranken.prsmith.basicactivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Setup Support Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Setup Floating Action Button (FAB)
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).show();
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            // Make the Home/Up button behave the same as the back button
+            onBackPressed();
+            return true;
+        }
+        if (id == R.id.action_settings) {
+            // TODO: implement settings activity
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
