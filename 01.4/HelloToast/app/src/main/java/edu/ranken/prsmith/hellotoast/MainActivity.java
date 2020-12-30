bbd11d2fb0994c5644e68239722412350009b20b
Paul Smith <prsmith@ranken.edu>
2020-12-30 16:35:17 -0600

    Adding launcher icon for task 1

---
+++ 01.4/HelloToast/app/src/main/java/edu/ranken/prsmith/hellotoast/MainActivity.java
@@ -0,0 +1,32 @@
+package edu.ranken.prsmith.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int _count;
+    private TextView _countText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        _countText = findViewById(R.id.text_count);
+        _countText.setText(String.valueOf(_count));
+    }
+
+    public void showToast(View view) {
+        Toast.makeText(this, R.string.toast_message, Toast.LENGTH_LONG).show();
+    }
+
+    public void countUp(View view) {
+        ++_count;
+        _countText.setText(String.valueOf(_count));
+    }
+}
