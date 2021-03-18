5f0eb6cbef3fedc7911e76c70e31188eb3e3267d
Paul Smith <prsmith@ranken.edu>
2021-03-18 11:24:54 -0500

    image upload wip

---
+++ 08.3/ImageUpload/app/src/main/java/edu/ranken/prsmith/imageupload/worker/UploadWorker.java
@@ -0,0 +1,7 @@
+package edu.ranken.prsmith.imageupload.worker;
+
+public class UploadWorker {
+    private static final String LOG_TAG = "ImageUpload";
+    public static final String PARAM_SOURCE = "source";
+    public static final String PARAM_DESTINATION = "destination";
+}
