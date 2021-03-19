e4ff713116893e483cdfe9ead43b38c4f27046a0
Paul Smith <prsmith@ranken.edu>
2021-03-19 13:02:57 -0500

    fortune cookie created

---
+++ 08.3/FortuneCookie/app/src/main/java/edu/ranken/prsmith/fortunecookie/FortuneCookieApp.java
@@ -0,0 +1,11 @@
+package edu.ranken.prsmith.fortunecookie;
+
+import android.app.Application;
+
+public class FortuneCookieApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ 08.3/FortuneCookie/app/src/main/java/edu/ranken/prsmith/fortunecookie/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package edu.ranken.prsmith.fortunecookie.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.ranken.prsmith.fortunecookie.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
